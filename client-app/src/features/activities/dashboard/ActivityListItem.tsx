d990e6aa295bd121b5c8d98a0f510e9a7ad97f83
brian <weimin@clare.ai>
2022-03-09 20:05:08 +0800

    Implemented Client image upload

--- client-app/src/features/activities/dashboard/ActivityListItem.tsx
+++ client-app/src/features/activities/dashboard/ActivityListItem.tsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import { Button, Icon, Item, Label, Segment } from "semantic-ui-react";
 import { Activity } from "../../../app/models/activity";
-import {format} from 'date-fns';
+import { format } from 'date-fns';
 import ActivityListItemAttendee from "./ActivityListItemAttendee";
 
 interface Props {
@@ -16,16 +16,18 @@ function ActivityListItem(props: Props) {
       <Segment>
         {
           activity.isCancelled &&
-          <Label attached="top" color="red" content='Cancelled' style={{textAlign: 'center'}} />
+          <Label attached="top" color="red" content='Cancelled' style={{ textAlign: 'center' }} />
         }
         <Item.Group>
           <Item>
-            <Item.Image style={{marginBottom: 5}} size='tiny' circular src='/assets/user.png' />
+            <Item.Image style={{ marginBottom: 5 }} size='tiny' circular src={activity.host?.image || '/assets/user.png'} />
             <Item.Content>
               <Item.Header as={Link} to={`/activities/${activity.id}`} >
                 {activity.title}
               </Item.Header>
-              <Item.Description>Hosted by {activity.host?.displayName}</Item.Description>
+              <Item.Description>
+                Hosted by <Link to={`/profiles/${activity.hostUsername}`}>{activity.host?.displayName}</Link> 
+              </Item.Description>
               {
                 activity.isHost && (
                   <Item.Description>
