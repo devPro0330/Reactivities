5890562a71a8152a57931699f3102906449a9b77
brian <weimin@clare.ai>
2022-03-22 21:02:07 +0800

    Added followers & following feature

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -1,7 +1,7 @@
 using Application.Core;
+using Application.interfaces;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
-using Domain;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
@@ -15,8 +15,10 @@ namespace Application.Activities
     {
       private readonly DataContext _context;
       private readonly IMapper _mapper;
-      public Handler(DataContext context, IMapper mapper)
+      private readonly IUserAccessor _userAccessor;
+      public Handler(DataContext context, IMapper mapper, IUserAccessor userAccessor)
       {
+        _userAccessor = userAccessor;
         _mapper = mapper;
         _context = context;
       }
@@ -24,7 +26,7 @@ namespace Application.Activities
       public async Task<Result<List<ActivityDto>>> Handle(Query request, CancellationToken cancellationToken)
       {
         var activities = await _context.Activities
-          .ProjectTo<ActivityDto>(_mapper.ConfigurationProvider)
+          .ProjectTo<ActivityDto>(_mapper.ConfigurationProvider, new {currentUsername = _userAccessor.GetUserName()})
           .ToListAsync(cancellationToken);
 
         return Result<List<ActivityDto>>.Success(activities);
